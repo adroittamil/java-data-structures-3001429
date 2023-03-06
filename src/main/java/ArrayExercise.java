70b1eb8b596f8d0ab54357ec602d7cd2833bb57a
tamilselvanramalingam <adroittamil@gmail.com>
2023-03-05 19:22:34 -0500

    Test commit

--- src/main/java/ArrayExercise.java
+++ src/main/java/ArrayExercise.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 public class ArrayExercise {
 
   public void arrayToUpperCase(String[] input) {
     for(int i = 0; i < input.length; i++) {
       input[i] = input[i].toUpperCase();
     }
+     Arrays.stream(input).forEach(System.out::println);
   }
 
   public void incrementArray(int[] input) {
     // Add code here to increment each element in the array by 1
+    for ( int i =0; i< input.length;i++) {
+      input[i]=input[i]+1;
+    }
   }
 
 }
